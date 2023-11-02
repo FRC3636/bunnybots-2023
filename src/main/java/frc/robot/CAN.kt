3e1f636612cde2e409fe66d2618a63c1cb4e3df3
rotating.cow <ben.arkonovich@gmail.com>
2023-11-02 14:41:30 -0700

    refactor(CAN): change id's
    asa says hi

--- src/main/java/frc/robot/CAN.kt
+++ src/main/java/frc/robot/CAN.kt
@@ -1,14 +1,14 @@
 package frc.robot
 
 enum class CANDevice(val id: Int) {
-    FrontLeftTurningMotor(10),
-    FrontLeftDrivingMotor(11),
-    FrontRightTurningMotor(12),
-    FrontRightDrivingMotor(13),
-    BackLeftTurningMotor(14),
-    BackLeftDrivingMotor(15),
-    BackRightTurningMotor(16),
-    BackRightDrivingMotor(17),
+    FrontLeftTurningMotor(4),
+    FrontLeftDrivingMotor(3),
+    FrontRightTurningMotor(6),
+    FrontRightDrivingMotor(5),
+    BackLeftTurningMotor(2),
+    BackLeftDrivingMotor(1),
+    BackRightTurningMotor(7),
+    BackRightDrivingMotor(8),
     TurretMotor(18),
     YeetWheelMotor(19),
     YeetFeedMotor(20),
