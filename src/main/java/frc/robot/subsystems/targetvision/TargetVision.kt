6e8806d20c14edfade88993db287eecd06347f99
rotating.cow <ben.arkonovich@gmail.com>
2023-11-02 21:09:43 -0700

    Revert "unfuck /fuck up the shit but isolated fuckery"
    This reverts commit e6f1663a3e8d044dcd53d5e85e50372bcd2a94ff.
    
    wrong branch lmao

--- src/main/java/frc/robot/subsystems/targetvision/TargetVision.kt
+++ src/main/java/frc/robot/subsystems/targetvision/TargetVision.kt
@@ -1,84 +1,84 @@
 package frc.robot.subsystems.targetvision
-//
-//
-//import edu.wpi.first.math.geometry.Translation2d
-//import edu.wpi.first.math.util.Units
-//import edu.wpi.first.wpilibj2.command.Subsystem
-//import frc.robot.utils.LimelightHelpers.LimelightTarget_Retro
-//import org.littletonrobotics.junction.Logger
-//import kotlin.math.tan
-//
-//object TargetVision:  Subsystem {
-//
-//    private val io: TargetVisionIO = Limelight
-//    private val inputs: TargetVisionIO.TargetVisionIOInputs = TargetVisionIO.TargetVisionIOInputs()
-//    private const val SAMPLE_NUM= 8
-//
-//
-//    fun getDistance(target: LimelightTarget_Retro): Double{
-//        val angleToBucket = Units.degreesToRadians(CAMERA_PITCH + target.ty)
-//        return (BUCKET_HEIGHT - LIMELIGHT_HEIGHT) / tan(angleToBucket)
-//    }
-//
-//    //ascending
-//    private val targetsbyDistance: List<Pair<LimelightTarget_Retro, Double>>
-//        get() {
-//            return inputs.targets.map {target: LimelightTarget_Retro -> Pair(target, getDistance(target))}.sortedBy {it.second}
-//        }
-//    val hasTargets: Boolean
-//        get(){
-//            return inputs.hasTargets
-//        }
-//    val curTime: Double
-//        get() {return inputs.lastTimeStampMS}
-//
-//
-//    override fun periodic() {
-//        Logger.getInstance().processInputs("Vision", inputs)
-//        if (hasTargets) {
-//            addMeasurement(takeMeasurement(closestTarget))
-//        }
-//        io.updateInputs(inputs)
-//    }
-//
-//    data class Measurement(val timestamp: Double, val pose: Translation2d)
-//
-//    private fun takeMeasurement(target: LimelightTarget_Retro): Measurement{
-//
-//        val targetTranslation = Translation2d(getDistance(target), target.tx)
-//
-//        return Measurement(inputs.lastTimeStampMS, targetTranslation)
-//
-//    }
-//
-//
-//    val closestTarget: LimelightTarget_Retro
-//        get(){
-//            return targetsbyDistance.map{pair: Pair<LimelightTarget_Retro, Double> -> pair.first}[0]
-//        }
-//
-//    // samples
-//    var smaples: MutableList<Measurement> = mutableListOf()
-//
-//
-//    fun addMeasurement(measurement: Measurement){
-//        smaples.add(measurement)
-//
-//        if (smaples.size > SAMPLE_NUM)
-//            smaples.removeFirst()
-//
-//    }
-//
-//    fun reset(){
-//        smaples = mutableListOf()
-//    }
-//
-//
-//
-//
-//     private const val CAMERA_PITCH = 110 // degrees
-//     private const val LIMELIGHT_HEIGHT = 27.33 // inches
-//     private const val BUCKET_HEIGHT = 49 // inches. rough guess.// TODO unrough the guess
-//
-//
-//}
+
+
+import edu.wpi.first.math.geometry.Translation2d
+import edu.wpi.first.math.util.Units
+import edu.wpi.first.wpilibj2.command.Subsystem
+import frc.robot.utils.LimelightHelpers.LimelightTarget_Retro
+import org.littletonrobotics.junction.Logger
+import kotlin.math.tan
+
+object TargetVision:  Subsystem {
+
+    private val io: TargetVisionIO = Limelight
+    private val inputs: TargetVisionIO.TargetVisionIOInputs = TargetVisionIO.TargetVisionIOInputs()
+    private const val SAMPLE_NUM= 8
+
+
+    fun getDistance(target: LimelightTarget_Retro): Double{
+        val angleToBucket = Units.degreesToRadians(CAMERA_PITCH + target.ty)
+        return (BUCKET_HEIGHT - LIMELIGHT_HEIGHT) / tan(angleToBucket)
+    }
+
+    //ascending
+    private val targetsbyDistance: List<Pair<LimelightTarget_Retro, Double>>
+        get() {
+            return inputs.targets.map {target: LimelightTarget_Retro -> Pair(target, getDistance(target))}.sortedBy {it.second}
+        }
+    val hasTargets: Boolean
+        get(){
+            return inputs.hasTargets
+        }
+    val curTime: Double
+        get() {return inputs.lastTimeStampMS}
+
+
+    override fun periodic() {
+        Logger.getInstance().processInputs("Vision", inputs)
+        if (hasTargets) {
+            addMeasurement(takeMeasurement(closestTarget))
+        }
+        io.updateInputs(inputs)
+    }
+
+    data class Measurement(val timestamp: Double, val pose: Translation2d)
+
+    private fun takeMeasurement(target: LimelightTarget_Retro): Measurement{
+
+        val targetTranslation = Translation2d(getDistance(target), target.tx)
+
+        return Measurement(inputs.lastTimeStampMS, targetTranslation)
+
+    }
+
+
+    val closestTarget: LimelightTarget_Retro
+        get(){
+            return targetsbyDistance.map{pair: Pair<LimelightTarget_Retro, Double> -> pair.first}[0]
+        }
+
+    // samples
+    var smaples: MutableList<Measurement> = mutableListOf()
+
+
+    fun addMeasurement(measurement: Measurement){
+        smaples.add(measurement)
+
+        if (smaples.size > SAMPLE_NUM)
+            smaples.removeFirst()
+
+    }
+
+    fun reset(){
+        smaples = mutableListOf()
+    }
+
+
+
+
+     private const val CAMERA_PITCH = 110 // degrees
+     private const val LIMELIGHT_HEIGHT = 27.33 // inches
+     private const val BUCKET_HEIGHT = 49 // inches. rough guess.// TODO unrough the guess
+
+
+}
--- src/main/java/frc/robot/subsystems/turret/Turret.kt
+++ src/main/java/frc/robot/subsystems/turret/Turret.kt
@@ -1,67 +1,67 @@
 package frc.robot.subsystems.turret
-//
-//import edu.wpi.first.math.controller.SimpleMotorFeedforward
-//import edu.wpi.first.math.geometry.Rotation2d
-//import edu.wpi.first.wpilibj.RobotBase
-//import edu.wpi.first.wpilibj2.command.Subsystem
-//import frc.robot.CANDevice
-//import frc.robot.subsystems.drivetrain.Drivetrain
-//import frc.robot.utils.PIDCoefficients
-//import frc.robot.utils.PIDController
-//import kotlin.math.absoluteValue
-//import kotlin.math.sign
-//
-//
-//object Turret : Subsystem {
-//
-//    val pidController = PIDController(PIDCoefficients(0.0,0.0,0.0))
-//
-//    val feedForward = SimpleMotorFeedforward(0.0,0.0,0.0)
-//
-//    val io = if(RobotBase.isReal()){
-//        TurretIOReal(CANDevice.TurretMotor)
-//    }else{
-//        TurretIOSim()
-//    }
-//
-//
-//    private val turretInputs = TurretInputs()
-//
-//    const val MAX_ROTATION_DEGREES = 90.0
-//    // factoring in rotation of drivetrain
-//    private var targetRotation: Rotation2d = Rotation2d()
-//        set(value) {
-//            var degrees = value.degrees % 360
-//
-//            if(degrees.absoluteValue > 180) {
-//                degrees -= 360 * degrees.sign
-//            }
-//            degrees = degrees.coerceIn(-MAX_ROTATION_DEGREES, MAX_ROTATION_DEGREES)
-//            field = Rotation2d.fromDegrees(degrees)
-//        }
-//
-//
-//    override fun periodic() {
-//
-//    }
-//
-//    fun aim() {
-//        io.setVoltage(
-//                pidController.calculate(relativeAngle.radians, targetRotation.radians)
-//                        + feedForward.calculate(Drivetrain.velocity2d.norm)
-//        )
-//
-//    }
-//
-//    fun setTarget(target: Rotation2d){
-//        targetRotation = target
-//    }
-//    val relativeAngle: Rotation2d
-//        get() = Rotation2d(turretInputs.position.radians + Drivetrain.rotation.radians)
-//
-//
-//
-//
-//
-//
-//}
+
+import edu.wpi.first.math.controller.SimpleMotorFeedforward
+import edu.wpi.first.math.geometry.Rotation2d
+import edu.wpi.first.wpilibj.RobotBase
+import edu.wpi.first.wpilibj2.command.Subsystem
+import frc.robot.CANDevice
+import frc.robot.subsystems.drivetrain.Drivetrain
+import frc.robot.utils.PIDCoefficients
+import frc.robot.utils.PIDController
+import kotlin.math.absoluteValue
+import kotlin.math.sign
+
+
+object Turret : Subsystem {
+
+    val pidController = PIDController(PIDCoefficients(0.0,0.0,0.0))
+
+    val feedForward = SimpleMotorFeedforward(0.0,0.0,0.0)
+
+    val io = if(RobotBase.isReal()){
+        TurretIOReal(CANDevice.TurretMotor)
+    }else{
+        TurretIOSim()
+    }
+
+
+    private val turretInputs = TurretInputs()
+
+    const val MAX_ROTATION_DEGREES = 90.0
+    // factoring in rotation of drivetrain
+    private var targetRotation: Rotation2d = Rotation2d()
+        set(value) {
+            var degrees = value.degrees % 360
+           
+            if(degrees.absoluteValue > 180) {
+                degrees -= 360 * degrees.sign
+            }
+            degrees = degrees.coerceIn(-MAX_ROTATION_DEGREES, MAX_ROTATION_DEGREES)
+            field = Rotation2d.fromDegrees(degrees)
+        }
+
+
+    override fun periodic() {
+
+    }
+
+    fun aim() {
+        io.setVoltage(
+                pidController.calculate(relativeAngle.radians, targetRotation.radians)
+                        + feedForward.calculate(Drivetrain.velocity2d.norm)
+        )
+
+    }
+
+    fun setTarget(target: Rotation2d){
+        targetRotation = target
+    }
+    val relativeAngle: Rotation2d
+        get() = Rotation2d(turretInputs.position.radians + Drivetrain.rotation.radians)
+
+
+
+
+
+
+}
