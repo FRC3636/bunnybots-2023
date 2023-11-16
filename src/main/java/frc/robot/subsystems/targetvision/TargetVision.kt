b0af39d231ce81459dff7ee32b5008d7c48a671d
Max Niederman <max@maxniederman.com>
2023-11-16 11:22:00 -0800

    refactor(targetvision): simplify IO interface, remove duplicate and unnecessary code, use SI units

--- src/main/java/frc/robot/subsystems/targetvision/TargetVision.kt
+++ src/main/java/frc/robot/subsystems/targetvision/TargetVision.kt
@@ -1,8 +1,10 @@
 package frc.robot.subsystems.targetvision
 
 
+import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.util.Units
+import edu.wpi.first.math.util.Units.inchesToMeters
 import edu.wpi.first.wpilibj2.command.Subsystem
 import frc.robot.utils.LimelightHelpers.LimelightTarget_Retro
 import org.littletonrobotics.junction.Logger
@@ -10,73 +12,50 @@ import kotlin.math.tan
 
 object TargetVision : Subsystem {
     private val io: TargetVisionIO = Limelight
-    private val inputs: TargetVisionIO.TargetVisionIOInputs = TargetVisionIO.TargetVisionIOInputs()
+    private val inputs: TargetVisionIO.Inputs = TargetVisionIO.Inputs()
     private const val SAMPLE_NUM = 8
 
-    fun getDistance(target: LimelightTarget_Retro): Double {
-        val angleToBucket = Units.degreesToRadians(CAMERA_PITCH + target.ty)
+    override fun periodic() {
+        io.updateInputs(inputs)
+        Logger.getInstance().processInputs("Vision", inputs)
+
+        if (inputs.targets.isNotEmpty()) {
+            addMeasurement(takeMeasurement(targetsByDistance[0].first))
+        }
+    }
+
+    // in ascending order
+    private val targetsByDistance: List<Pair<LimelightTarget_Retro, Double>>
+        get() {
+            return inputs.targets.map { target: LimelightTarget_Retro -> Pair(target, distanceToTarget(target)) }
+                .sortedBy { it.second }
+        }
+
+    private var measurements: MutableList<Measurement> = mutableListOf()
+
+    data class Measurement(val timestamp: Double, val translation: Translation2d)
+
+    private fun takeMeasurement(target: LimelightTarget_Retro): Measurement {
+        val targetTranslation = Translation2d(distanceToTarget(target), target.tx)
+
+        return Measurement(timestamp = inputs.lastUpdateTimestamp, translation = targetTranslation)
+    }
+
+    private fun addMeasurement(measurement: Measurement) {
+        measurements.add(measurement)
+
+        if (measurements.size > SAMPLE_NUM) {
+            measurements.removeFirst()
+        }
+    }
+
+    private fun distanceToTarget(target: LimelightTarget_Retro): Double {
+        val angleToBucket = Units.degreesToRadians(CAMERA_PITCH.degrees + target.ty)
         return (BUCKET_HEIGHT - LIMELIGHT_HEIGHT) / tan(angleToBucket)
     }
 
-    //ascending
-    private val targetsbyDistance: List<Pair<LimelightTarget_Retro, Double>>
-        get() {
-            return inputs.targets.map { target: LimelightTarget_Retro -> Pair(target, getDistance(target)) }
-                .sortedBy { it.second }
-        }
-    val hasTargets: Boolean
-        get() {
-            return inputs.hasTargets
-        }
-    val curTime: Double
-        get() {
-            return inputs.lastTimeStampMS
-        }
-
-
-    override fun periodic() {
-        Logger.getInstance().processInputs("Vision", inputs)
-        if (hasTargets) {
-            addMeasurement(takeMeasurement(closestTarget))
-        }
-        io.updateInputs(inputs)
-    }
-
-    data class Measurement(val timestamp: Double, val pose: Translation2d)
-
-    private fun takeMeasurement(target: LimelightTarget_Retro): Measurement {
-
-        val targetTranslation = Translation2d(getDistance(target), target.tx)
-
-        return Measurement(inputs.lastTimeStampMS, targetTranslation)
-
-    }
-
-
-    val closestTarget: LimelightTarget_Retro
-        get() {
-            return targetsbyDistance.map { pair: Pair<LimelightTarget_Retro, Double> -> pair.first }[0]
-        }
-
-    // samples
-    var smaples: MutableList<Measurement> = mutableListOf()
-
-
-    fun addMeasurement(measurement: Measurement) {
-        smaples.add(measurement)
-
-        if (smaples.size > SAMPLE_NUM)
-            smaples.removeFirst()
-
-    }
-
-    fun reset() {
-        smaples = mutableListOf()
-    }
-
-    private const val CAMERA_PITCH = 110 // degrees
-    private const val LIMELIGHT_HEIGHT = 27.33 // inches
-    private const val BUCKET_HEIGHT = 49 // inches. rough guess.// TODO unrough the guess
-
-
+    // Constants
+    private val CAMERA_PITCH = Rotation2d.fromDegrees(110.0)
+    private val LIMELIGHT_HEIGHT = inchesToMeters(27.33)
+    private val BUCKET_HEIGHT = inchesToMeters(49.0) // TODO: this is a rough guess, measure exactly
 }
--- src/main/java/frc/robot/subsystems/targetvision/TargetVisionIO.kt
+++ src/main/java/frc/robot/subsystems/targetvision/TargetVisionIO.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import edu.wpi.first.networktables.NetworkTableEvent
 import edu.wpi.first.networktables.NetworkTableEvent.Kind
 import edu.wpi.first.networktables.NetworkTableInstance
+import edu.wpi.first.wpilibj.Timer
 import frc.robot.utils.LimelightHelpers
 import frc.robot.utils.LimelightHelpers.LimelightResults
 import org.littletonrobotics.junction.LogTable
@@ -13,82 +14,55 @@ import java.util.*
 
 interface TargetVisionIO {
 
-    class TargetVisionIOInputs : LoggableInputs {
+    class Inputs : LoggableInputs {
+        var currentTimestamp: Double = 0.0
+        var lastUpdateTimestamp: Double = 0.0
 
-
-        var hasTargets: Boolean = false
-        var targetCount = 0
+        // FIXME: this isn't being logged, our turret will be useless in replay
         var targets: List<LimelightHelpers.LimelightTarget_Retro> = mutableListOf()
-        var latencyms: Double = 0.0
-        var lastTimeStampMS: Double = 0.0
 
 
         override fun toLog(table: LogTable?) {
-            table?.put("HasTargets", hasTargets)
-            table?.put("TargetCount", targetCount.toDouble())
-            table?.put("lastTimeStamp", lastTimeStampMS)
+            table?.put("Current Timestamp", currentTimestamp)
+            table?.put("Last Update Timestamp", lastUpdateTimestamp)
         }
 
         override fun fromLog(table: LogTable?) {
-            table?.getBoolean("HasTargets", hasTargets)?.let { hasTargets = it }
-            table?.getDouble("TargetCount", targetCount.toDouble())?.let { targetCount = it.toInt() }
-            table?.getDouble("lastTimeStamp", lastTimeStampMS)?.let { lastTimeStampMS = it }
-
+            table?.getDouble("Current Timestamp", 0.0)?.let { currentTimestamp = it }
+            table?.getDouble("Last Update Timestamp", 0.0)?.let { lastUpdateTimestamp = it }
         }
-
     }
 
-    fun updateInputs(inputs: TargetVisionIOInputs)
-
+    fun updateInputs(inputs: Inputs)
 }
 
 object Limelight : TargetVisionIO {
-    private var hasTargets = false
-    private var targets: List<LimelightHelpers.LimelightTarget_Retro> = mutableListOf()
-    private var targetCount = 0
-    private var lastTimeStamp: Double = 0.0
+    private val nt = NetworkTableInstance.getDefault()
+
+    private var targets: List<LimelightHelpers.LimelightTarget_Retro> = emptyList()
+    private var lastUpdateTimestamp = 0.0
 
     private val table = NetworkTableInstance.getDefault().getTable("limelight")
     private val dumpSubscriber = table.getStringTopic("json").subscribe("no json :(")
-    private val cl = table.getDoubleTopic("cl").subscribe(0.0)
-    private val tl = table.getDoubleTopic("tl").subscribe(0.0)
-    private val ts = table.getStringTopic("ts").subscribe(":3")
 
-    val curTimestamp: Double
-        get() {
-            val updates = ts.readQueue()
-            val lastUpdate = updates[updates.size - 1]
-            val latency: Double = cl.get() + tl.get()
-            return (lastUpdate.timestamp * 1e-6) * (latency * 1e-3)
-        }
-
-    override fun updateInputs(inputs: TargetVisionIO.TargetVisionIOInputs) {
-
-        inputs.hasTargets = hasTargets
+    override fun updateInputs(inputs: TargetVisionIO.Inputs) {
+        inputs.currentTimestamp = Timer.getFPGATimestamp() + nt.serverTimeOffset.orElse(0)
+        inputs.lastUpdateTimestamp = lastUpdateTimestamp
         inputs.targets = targets
-        inputs.lastTimeStampMS = curTimestamp
-
     }
 
 
     init {
-        val nt = NetworkTableInstance.getDefault()
-
         nt.addListener(
-            dumpSubscriber,
-            EnumSet.of(Kind.kValueAll),
-            ::eventListener
+            dumpSubscriber, EnumSet.of(Kind.kValueAll), ::eventListener
         )
-
     }
 
 
     private fun eventListener(event: NetworkTableEvent) {
         val results = ObjectMapper().readValue(dumpSubscriber.get(), LimelightResults::class.java)
-        hasTargets = results.targetingResults.targets_Retro.isNotEmpty()
+        lastUpdateTimestamp = results.targetingResults.timestamp_RIOFPGA_capture
         targets = results.targetingResults.targets_Retro.toList()
-        targetCount = results.targetingResults.targets_Retro.size
-
     }
 }
 
