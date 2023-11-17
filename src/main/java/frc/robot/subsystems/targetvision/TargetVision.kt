f8d19fb0cd42a0d89eaf886dd77bd3be076bdcb2
rotating.cow <ben.arkonovich@gmail.com>
2023-11-17 14:22:09 -0800

    feat(vision): Finish target grouping

--- src/main/java/frc/robot/subsystems/targetvision/TargetVision.kt
+++ src/main/java/frc/robot/subsystems/targetvision/TargetVision.kt
@@ -1,6 +1,7 @@
 package frc.robot.subsystems.targetvision
 
 
+import edu.wpi.first.math.Num
 import edu.wpi.first.math.geometry.Translation2d
 import edu.wpi.first.math.util.Units
 import edu.wpi.first.wpilibj.DriverStation
@@ -10,6 +11,8 @@ import org.littletonrobotics.junction.Logger
 import kotlin.math.abs
 import kotlin.math.tan
 import kotlin.math.pow
+import edu.wpi.first.math.Vector
+
 
 object TargetVision:  Subsystem {
 
@@ -18,7 +21,7 @@ object TargetVision:  Subsystem {
     private const val SAMPLE_NUM = 8
 
 
-    fun getDistance(target: LimelightTarget_Detector): Double{
+    private fun getDistance(target: LimelightTarget_Detector): Double{
         val angleToBucket = Units.degreesToRadians(CAMERA_PITCH + target.ty)
         return (BUCKET_HEIGHT - LIMELIGHT_HEIGHT) / tan(angleToBucket)
     }
@@ -52,6 +55,8 @@ object TargetVision:  Subsystem {
 
    data class Measurement(val timestamp: Double, val pose: LimelightTarget_Detector)
 
+    data class Sample(val timestamp : Double, val pose: Translation2d)
+
     private fun takeMeasurement(target: LimelightTarget_Detector): Measurement{
 
        //val targetTranslation = Translation2d(getDistance(target), target.tx)
@@ -61,15 +66,19 @@ object TargetVision:  Subsystem {
    }
 
 
-    val closestTarget: LimelightTarget_Detector
+
+    val primaryTargetSamples: List<Sample>
         get(){
-            return targetsbyDistance.map{pair: Pair<LimelightTarget_Detector, Double> -> pair.first}[0]
+            return robotPoses[0].map { pair -> Sample(pair.timestamp, Translation2d(getDistance(pair.pose), pair.pose.tx))}
         }
 
-   // samples
-   var smaples: MutableList<Measurement> = mutableListOf()
 
-    // grouping
+    val primaryTarget: LimelightTarget_Detector
+        get(){
+            return robotPoses[0].last().pose
+        }
+
+
     var robotPoses: MutableList<MutableList<Measurement>> = mutableListOf()
 
 
@@ -81,8 +90,8 @@ object TargetVision:  Subsystem {
         val deltaY = abs(measurement.ty - last.ty)
         val deltaA = abs(measurement.ta.pow(0.5) - last.ta.pow(0.5))
 
-        score += (TX_WEIGHT)*( 1 - (deltaX.pow(TX_DROPOFF_RATE) / 27.0.pow(TX_DROPOFF_RATE)) ) //27 = horizontal fov of limelight
-        score += (TY_WEIGHT)*( 1 - (deltaY.pow(TY_DROPOFF_RATE) / 20.5.pow(TY_DROPOFF_RATE)) ) //20.5 = vertical fov of limelight
+        score += (TX_WEIGHT)*( 1 - (deltaX.pow(TX_DROPOFF_RATE) / 54.0.pow(TX_DROPOFF_RATE)) ) //27 = horizontal fov of limelight
+        score += (TY_WEIGHT)*( 1 - (deltaY.pow(TY_DROPOFF_RATE) / 41.0.pow(TY_DROPOFF_RATE)) ) //20.5 = vertical fov of limelight
         score += (TA_WEIGHT)*( 1 - (deltaA.pow(TA_DROPOFF_RATE) / TA_MAX.pow(TA_DROPOFF_RATE)) )
 
         return score
@@ -107,8 +116,6 @@ object TargetVision:  Subsystem {
             } else if((measurement.timestamp - pose.last().timestamp > MAX_SAMPLE_TIME_MS)){
                 robotPoses.remove(pose)
             }
-
-
         }
 
         if(!grouped){
@@ -116,22 +123,41 @@ object TargetVision:  Subsystem {
         }
 
 
-        robotPoses = robotPoses.sortedBy {rateTrackingPreference(it)} as MutableList<MutableList<Measurement>>
 
-   }
 
-   fun reset(){
-       smaples = mutableListOf()
+
    }
 
 
     //give a measurement a rating based on how likely it is to be our current primary target
-    private fun rateTrackingPreference(measurement: List<Measurement>): Double{
+    private fun rateTrackingPreference(measurement: List<List<Measurement>>): Int{
 
-        var score: Double = 0.0
+        fun getVelocity(samples: Pair<List<Measurement>, Int>): Double{
+            val tVector: List<Double> = samples.first.map{(timestamp, _) -> timestamp}
+            val txVector: List<Double> = samples.first.map{(_, target) -> target.tx}
+
+            // regressing tx
+            return tVector.zip(txVector).sumOf { pair -> pair.first * pair.second } /
+                    tVector.zip(tVector).sumOf { pair -> pair.first * pair.second }
+
+        }
+
+        var measurementIndexed = measurement.map { list -> Pair(list, measurement.indexOf(list))}
+
+        //filter by tx
+        measurementIndexed = measurementIndexed.sortedBy { pair -> abs(pair.first.last().pose.tx)}
+        val lowestTx = abs(measurementIndexed[0].first.last().pose.tx)
+        measurementIndexed = measurementIndexed.filter{ pair -> abs(pair.first.last().pose.tx) in lowestTx..lowestTx+TX_THRESHOLD}
+
+        //filter by distance
+        measurementIndexed = measurementIndexed.sortedBy {pair -> getDistance(pair.first.last().pose)}
+        val lowestDistance = getDistance(measurementIndexed[0].first.last().pose)
+        measurementIndexed = measurementIndexed.filter{ pair -> getDistance(pair.first.last().pose) in lowestDistance..lowestDistance + DISTANCE_THRESHOLD}
 
 
-        return score
+        return measurementIndexed.minByOrNull { getVelocity(it) }!!.second
+
+
     }
 
 
@@ -167,6 +193,10 @@ object TargetVision:  Subsystem {
     private const val ANGLE_WEIGHT = 0.70
     private const val ANGULAR_VELOCITY_WEIGHT = 0.10
     private const val DISTANCE_WEIGHT = 0.20
+
+    private const val TX_THRESHOLD = 5.0
+    private const val DISTANCE_THRESHOLD = 35.0 //inches
+
 
 
 
