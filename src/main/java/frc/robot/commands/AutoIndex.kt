80ef8354d2f0d11eb3f81fd3d00bb0e173aa6888
Craftzman7 <zezetta@icloud.com>
2023-11-18 14:35:47 -0800

    fix(indexer): run auto indexing for set amount of time

--- src/main/java/frc/robot/commands/AutoIndex.kt
+++ src/main/java/frc/robot/commands/AutoIndex.kt
@@ -1,15 +1,24 @@
 package frc.robot.commands
 
+import edu.wpi.first.wpilibj.Timer
 import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.indexer.Indexer
 
 class AutoIndex : Command {
+    private val timer = Timer()
+    private var didTimerStart = false
+
     override fun execute() {
         if(!Indexer.beamBreak) {
+            if (!didTimerStart) timer.start()
             Indexer.setSpeed(1.0)
         } else {
             Indexer.setSpeed(0.0)
         }
+    }
+
+    override fun isFinished(): Boolean {
+        return timer.hasElapsed(7.0)
     }
 
     override fun getRequirements() = setOf(Indexer)
