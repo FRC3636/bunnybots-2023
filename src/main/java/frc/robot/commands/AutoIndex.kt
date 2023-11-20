cf3d2d7d1aaebc1cc18e844883c2f49ba0351295
doinkythederp <doinkythederp@icloud.com>
2023-11-19 21:44:16 -0800

    fix(auto-index): run the indexer for a set timeout

--- src/main/java/frc/robot/commands/AutoIndex.kt
+++ src/main/java/frc/robot/commands/AutoIndex.kt
@@ -5,25 +5,26 @@ import edu.wpi.first.wpilibj2.command.Command
 import frc.robot.subsystems.indexer.Indexer
 
 class AutoIndex : Command {
-    private val timer = Timer()
-    private var didTimerStart = false
+    private val forceIndexTimeout = Timer()
+    private var forcingIndex = false
 
     override fun execute() {
-        if (timer.hasElapsed(7.0)) {
-            Indexer.setSpeed(0.0)
-            timer.stop()
-            timer.reset()
-            didTimerStart = false
+        if (forcingIndex) {
+            Indexer.setSpeed(1.0)
+            return
         }
 
-        if(!Indexer.beamUnbroken) {
-            if (!didTimerStart) {
-                timer.start()
-                didTimerStart = true
-            }
-            Indexer.setSpeed(1.0)
-        } else {
+        if (forceIndexTimeout.hasElapsed(7.0)) {
+            forcingIndex = false
+            forceIndexTimeout.stop()
+            forceIndexTimeout.reset()
+        }
+
+        if(Indexer.beamUnbroken) {
             Indexer.setSpeed(0.0)
+        } else {
+            forcingIndex = true
+            forceIndexTimeout.start()
         }
     }
 
