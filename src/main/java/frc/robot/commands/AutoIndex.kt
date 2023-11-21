12079bc5267bc7c333fd1dc292627d76451d5d44
doinkythederp <doinkythederp@icloud.com>
2023-11-20 17:37:28 -0800

    refactor(AutoIndex): use sequential command groups

--- src/main/java/frc/robot/commands/AutoIndex.kt
+++ src/main/java/frc/robot/commands/AutoIndex.kt
@@ -1,31 +1,22 @@
 package frc.robot.commands
 
-import edu.wpi.first.wpilibj.Timer
-import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj2.command.*
 import frc.robot.subsystems.indexer.Indexer
 
-class AutoIndex : Command {
-    private val forceIndexTimeout = Timer()
-    private var forcingIndex = false
-
-    override fun execute() {
-        if (forceIndexTimeout.hasElapsed(7.0)) {
-            forcingIndex = false
-            forceIndexTimeout.stop()
-            forceIndexTimeout.reset()
-
-            if (!Indexer.objectDetected) {
-                Indexer.setSpeed(0.0)
-            }
-        }
-
-        if (Indexer.objectDetected && !forcingIndex) {
-            forcingIndex = true
-            forceIndexTimeout.start()
+class AutoIndex : SequentialCommandGroup(
+    object : Command {
+        override fun isFinished(): Boolean = Indexer.objectDetected
+        override fun getRequirements() = emptySet<Subsystem>()
+    },
+    object : SequentialCommandGroup(
+        InstantCommand ({
             Indexer.setSpeed(1.0)
-        }
+        }),
+        WaitCommand(7.0),
+        InstantCommand ({
+            Indexer.setSpeed(0.0)
+        }),
+    ) {
+        override fun getRequirements() = setOf(Indexer)
     }
-
-    override fun getRequirements() = setOf(Indexer)
-
-}
+)
