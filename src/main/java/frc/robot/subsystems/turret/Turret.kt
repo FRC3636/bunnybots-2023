af540bfd80e2e4df1561229ef365fd8fc4d701e4
doinkythederp <doinkythederp@icloud.com>
2023-11-22 13:28:15 -0800

    feat(Turret): add physics simulation (#19)
    Co-authored-by: drive station <drivestation@3636.team>

--- src/main/java/frc/robot/subsystems/turret/Turret.kt
+++ src/main/java/frc/robot/subsystems/turret/Turret.kt
@@ -4,6 +4,9 @@ import edu.wpi.first.math.controller.SimpleMotorFeedforward
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj2.command.Command
+import edu.wpi.first.wpilibj.smartdashboard.Mechanism2d
+import edu.wpi.first.wpilibj.smartdashboard.MechanismLigament2d
+import edu.wpi.first.wpilibj.smartdashboard.MechanismRoot2d
 import edu.wpi.first.wpilibj2.command.Subsystem
 import frc.robot.CANDevice
 import frc.robot.subsystems.drivetrain.Drivetrain
@@ -21,13 +24,18 @@ object Turret : Subsystem {
     private val io = if (RobotBase.isReal()) {
         TurretIOReal(CANDevice.TurretMotor)
     } else {
+        println("Using simulated turret")
         TurretIOSim()
     }
     private val inputs = TurretIO.Inputs()
 
-    private val pidController = PIDController(PIDCoefficients())
+    private val pidController = PIDController(PIDCoefficients(1.0, 0.0, 0.0))
 
     private val feedForward = SimpleMotorFeedforward(0.0, 0.0, 0.0)
+
+    private val mechanism = Mechanism2d(3.0, 3.0)
+    private val mechanismRoot: MechanismRoot2d = mechanism.getRoot("climber", 1.5, 1.5)
+    private val mechanismAim = mechanismRoot.append(MechanismLigament2d("aim", 3.0, inputs.angle.degrees))
 
     private var targetAngleToChassis: Rotation2d = Rotation2d()
         set(value) {
@@ -43,11 +51,13 @@ object Turret : Subsystem {
 
     override fun periodic() {
         io.updateInputs(inputs)
+        mechanismAim.angle = inputs.angle.degrees
+        Logger.getInstance().recordOutput("Turret/Mechanism", mechanism)
         Logger.getInstance().processInputs("Turret", inputs)
 
         io.setVoltage(
             pidController.calculate(
-                angleToField.radians, targetAngleToChassis.radians
+                angleToChassis.radians, targetAngleToChassis.radians
             ) + feedForward.calculate(-Drivetrain.chassisSpeeds.omegaRadiansPerSecond)
         )
     }
