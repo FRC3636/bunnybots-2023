f297b3a63c4cc9fcc4926ba108d89448de8357d2
doinkythederp <doinkythederp@icloud.com>
2023-12-05 19:13:30 -0800

    style: optimize imports

--- src/main/java/frc/robot/RobotContainer.kt
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -1,6 +1,5 @@
 package frc.robot
 
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.RobotBase
