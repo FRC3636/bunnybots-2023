7af1034a5140e784ebad7a17f0a60b9561fd23be
doinkythederp <doinkythederp@icloud.com>
2023-12-14 14:55:13 -0800

    feat: add slow intake

--- src/main/java/frc/robot/RobotContainer.kt
+++ src/main/java/frc/robot/RobotContainer.kt
@@ -1,18 +1,13 @@
 package frc.robot
 
-import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.wpilibj.DriverStation
 import edu.wpi.first.wpilibj.Joystick
 import edu.wpi.first.wpilibj.RobotBase
 import edu.wpi.first.wpilibj.XboxController
 import edu.wpi.first.wpilibj.smartdashboard.Field2d
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
-import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard
-import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab
 import edu.wpi.first.wpilibj2.command.Command
-import edu.wpi.first.wpilibj2.command.FunctionalCommand
 import edu.wpi.first.wpilibj2.command.InstantCommand
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup
 import edu.wpi.first.wpilibj2.command.button.JoystickButton
 import edu.wpi.first.wpilibj2.command.button.Trigger
@@ -22,9 +17,6 @@ import frc.robot.subsystems.drivetrain.Drivetrain
 import frc.robot.subsystems.indexer.Indexer
 import frc.robot.subsystems.intake.BallIntake
 import frc.robot.subsystems.shooter.Shooter
-import frc.robot.subsystems.turret.Turret
-import java.time.Instant
-import kotlin.math.atan2
 
 
 object RobotContainer {
@@ -98,7 +90,8 @@ object RobotContainer {
                 Indexer.setSpeedCommand(-1.0)
             )
             .onFalse(
-                Indexer.setSpeedCommand(0.0))
+                Indexer.setSpeedCommand(0.0)
+            )
 
         JoystickButton(controller, XboxController.Button.kLeftBumper.value).onTrue(
             BallIntake.runRollersCommand(-1.0)
@@ -126,14 +119,25 @@ object RobotContainer {
                 Indexer.setSpeedCommand(0.0)
             )
 
-        Trigger { controller.pov == 90 }
+        Trigger { controller.pov == 180 }
             .onTrue(
                 SetIntakePosition(BallIntake.Position.Down.pose, BallIntake),
             )
 
-        Trigger { controller.pov == 270 }
+        Trigger { controller.pov == 0 }
             .onTrue(
                 SetIntakePosition(BallIntake.Position.Up.pose, BallIntake),
+            )
+
+        Trigger { controller.rightTriggerAxis >= 0.1 }
+            .onTrue(
+                SequentialCommandGroup(
+                    BallIntake.runRollersCommand(0.25),
+                    SetIntakePosition(BallIntake.Position.Down.pose, BallIntake),
+                )
+            )
+            .onFalse(
+                BallIntake.runRollersCommand(0.0)
             )
     }
 
